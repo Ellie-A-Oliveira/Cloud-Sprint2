21b76243861712538d66a530ed4b6818f14b50ab
Ellie de Oliveira <104087847+Ellie-A-Oliveira@users.noreply.github.com>
2024-11-08 20:32:51 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+set -e
+
+echo "Para essa instalação, é necessário ter o Git e o Docker instalados..."
+
 mkdir -p odontoprev-spring-api
 cd odontoprev-spring-api
 
@@ -6,3 +10,8 @@ echo "Clonando repositório do projeto..."
 rm -rf odontoprev-spring
 
 git clone https://github.com/IvesJc/odontoprev-spring.git
+
+mv ../Dockerfile ./odontoprev-spring/Dockerfile
+mv ../docker-compose.yml ./docker-compose.yml
+
+docker compose up -d
