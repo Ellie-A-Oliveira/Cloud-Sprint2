38af0e457b108719e3460565c80e201ba99187bb
Ellie de Oliveira <104087847+Ellie-A-Oliveira@users.noreply.github.com>
2024-11-08 20:28:53 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+mkdir -p odontoprev-spring-api
+cd odontoprev-spring-api
+
+echo "Clonando repositório do projeto..."
+
+rm -rf odontoprev-spring
+
+git clone https://github.com/IvesJc/odontoprev-spring.git
